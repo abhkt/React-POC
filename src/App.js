a35591372861e4de25600c23d6db60e785e5c0f9
abhkt <abhkt2611@gmail.com>
2021-10-26 10:49:19 +0530

    initial CRA boilerplate setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return <div className='App'>Welcom to React POC</div>;
+}
+
+export default App;
